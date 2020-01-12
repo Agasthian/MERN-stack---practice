8f9ab03f29afe90392e18c4248fdf188f99fe368
Agasthian <agasthian_r@hotmail.com>
2020-01-12 20:57:04 +0530

    Passport config

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'auth/google/callback',
+      callbackURL: '/auth/google/callback',
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
