7f8eefb7776f6de96b138650248231c60f2dd937
Agasthian <agasthian_r@hotmail.com>
2020-01-12 20:37:40 +0530

    Heroku deployment config

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  googleRedirectURL:
-    'https://warm-garden-64707.herokuapp.com/auth/google/callback'
+  cookieKey: process.env.COOKIE_KEY
 };
