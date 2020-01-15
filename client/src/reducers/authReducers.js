d1a24e3bed01cb78603f70fabbe62e3019718a2d
Agasthian <agasthian_r@hotmail.com>
2020-01-15 23:59:43 +0530

    Compeleted client side react-redux with react routes

---
+++ client/src/reducers/authReducers.js
@@ -0,0 +1,11 @@
+export default (state = {}, { type, payload }) => {
+  switch (type) {
+    // case typeName:
+    //   return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
+
+
