dd67e691b2f9e5e871b0b1334abda498f948a6ff
Agasthian <agasthian_r@hotmail.com>
2020-01-17 23:48:02 +0530

    Stripe payment intergrated into both client and server side and tested payment with billing amount

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express'); //Express Lib
 const mongoose = require('mongoose'); // Mongoose Lib
 const cookieSession = require('cookie-session'); //Cookie - Session Lib
 const passport = require('passport'); // Passport Lib
+const bodyParser = require('body-parser');
 const keys = require('./config/keys'); // keys.js fiels
 //Order of importing is important, add user model first and then passport
 require('./models/User'); // Including Mongoose User model class STEP 5
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI); //Connecting Mongoose Library to Monogo DB Inst
 
 const app = express(); // STEP 1
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +25,7 @@ app.use(passport.initialize()); //STEP - 7
 app.use(passport.session());
 
 require('./routes/authRoutes')(app); //Including the auth Route(Route folder) into index and calling it with express app - writen using short hand (the file returns a function and we are calling it with express app) - STEP 2
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
