a0f37e5d6e260cd7f2b4b25d2a18b8ea1a4a2046
Agasthian <agasthian_r@hotmail.com>
2020-01-18 16:38:52 +0530

    Adding client side app

--- index.js
+++ index.js
@@ -27,5 +27,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); //Including the auth Route(Route folder) into index and calling it with express app - writen using short hand (the file returns a function and we are calling it with express app) - STEP 2
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'productions') {
+  //Express will serve up productions assets
+  //like our main.js file or main.css file
+  app.use(express.static('/client/build'));
+
+  //Express will serve up index.html file
+  //if it doesnt  recoganize the routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
